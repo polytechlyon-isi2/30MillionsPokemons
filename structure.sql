977f67f72203d8e6d191668a42b4035583ac795c
LhopS <sachalhopital5@gmail.com>
2016-02-18 17:43:43 +0100

    Implement the complete db structure

--- structure.sql
+++ structure.sql
@@ -20,3 +20,24 @@ create table Users (
     adresse varchar(500),
     admin tinyint (1) not null
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table Historiques (
+    login varchar(100) not null,
+    idpkm integer not null,
+    dateAchat date not null,
+    qteAchat integer not null,
+    PRIMARY KEY (login, idpkm, dateAchat),
+    CONSTRAINT fk_loginHisto FOREIGN KEY (login)
+REFERENCES Users(login),
+    CONSTRAINT fk_idpkmHisto FOREIGN KEY (idpkm)
+REFERENCES Pokemons(idpkm)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table (
+    idpkm integer not null,
+    type ENUM('acier','combat','dragon','eau','electrique','fee','feu','glace','insecte','normal','plante','poison','psy','roche','sol','spectre','tenebre','vol'),
+    PRIMARY KEY (idpkm, type),
+    CONSTRAINT fk_idpkmType FOREIGN KEY (idpkm)
+REFERENCES Pokemons(idpkm)    
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+
