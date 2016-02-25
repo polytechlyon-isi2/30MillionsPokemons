0b2f88e66be2a59b6f594eaa3c411ff72d4f400c
LhopS <sachalhopital5@gmail.com>
2016-02-25 16:01:18 +0100

    Add Bootstrap

--- views/view.php
+++ views/view.php
@@ -2,23 +2,42 @@
 <html>
     <head>
         <meta charset="utf-8" />
+
         <link href="pokemon.css" rel="stylesheet" />
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
         <title>30 Millions de Pokemons - Home</title>
     </head>
-    <body>
-        <header>
-            <h1>30 Millions de Pokemons</h1>
-        </header>
 
-        <?php foreach ($articles as $article): ?>
-        <article>
-            <h2><?php echo $article['nom_pkm'] ?></h2>
-            <p><?php echo $article['description'] ?></p>
-        </article>
-        <?php endforeach ?>
+    <body>
+
+        <div class="container">
+
+            <header>
+                <h1>30 Millions de Pokemons</h1>
+            </header>
+
+            <div class="col-sm-12">
+                <br>
+                <ul class="list-group">
+
+                    <?php foreach ($articles as $article): ?>
+                    <li class="list-group-item">
+                        <article>
+                            <h2><?php echo $article['nom_pkm'] ?></h2>
+                            <p><?php echo $article['description'] ?></p>
+                        </article>
+                    </li>
+                    <?php endforeach ?>
+
+                </ul>
+            </div>
+
+        </div>
 
         <footer class="footer">
             TODO : include footer here ! 
         </footer>
+
     </body>
 </html>
