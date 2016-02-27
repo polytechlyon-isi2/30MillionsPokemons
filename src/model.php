7e8635dc29348577b1e8db96ed0fe78b0a2f4877
LhopS <sachalhopital5@gmail.com>
2016-02-27 12:06:22 +0100

    UNSTABLE COMMIT
    problem in the path name "30"

--- src/model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Return all articles
-function getArticles() {
-    $bdd = new PDO('mysql:host=localhost;dbname=30MillionsPokemons;charset=utf8', 'pokemons_user', 'secret');
-    $articles = $bdd->query('select * from Pokemons order by nom_pkm desc');
-    return $articles;
-}
