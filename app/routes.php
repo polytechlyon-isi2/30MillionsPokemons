9b8326e5c62787a1e406065879f0e931da91ca18
LhopS <sachalhopital5@gmail.com>
2016-03-06 19:32:53 +0100

    UNSTABLE COMMIT
    Work in progress

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,16 @@
 <?php
 
-// Home page
+/* Home page */
 $app->get('/', function () use ($app) {
     $types = $app['dao.types']->findAll();
     return $app['twig']->render('index.html.twig', array('categories' => $types));
-});
+})->bind('home');
+
+/* Category details */
+$app->get('/category/{id}', function ($id) use ($app) {
+    $allPokemons = " ";
+    $types = $app['dao.types']->findAll();
+    //$article = $app['dao.article']->find($id);
+    //$comments = $app['dao.comment']->findAllByArticle($id);
+    return $app['twig']->render('index.html.twig', array('categories' => $types));
+})->bind('category');
