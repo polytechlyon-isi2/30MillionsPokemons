2b920a6db3fc91f404425da5421312934f3d9ac8
Sacha Lhopital <p1504274@univ-lyon1.fr>
2016-03-07 13:17:13 +0100

    Fix routes problems
    [Doesn't work with Wamp]

--- app/routes.php
+++ app/routes.php
@@ -1,16 +1,15 @@
 <?php
 
-/* Home page */
+/* Home page 
+ * Display all categories */
 $app->get('/', function () use ($app) {
     $types = $app['dao.types']->findAll();
     return $app['twig']->render('index.html.twig', array('categories' => $types));
 })->bind('home');
 
-/* Category details */
+/* Category details 
+ * Display all articles of the category selected */
 $app->get('/category/{id}', function ($id) use ($app) {
     $allPokemons = " ";
-    $types = $app['dao.types']->findAll();
-    //$article = $app['dao.article']->find($id);
-    //$comments = $app['dao.comment']->findAllByArticle($id);
-    return $app['twig']->render('index.html.twig', array('categories' => $types));
+    return $app['twig']->render('category.html.twig', array('articles' => $allPokemons));
 })->bind('category');
