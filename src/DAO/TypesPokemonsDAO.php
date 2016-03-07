19bf1edadc282d255104168a8b07a6e04f5e176c
Sacha Lhopital <p1504274@univ-lyon1.fr>
2016-03-07 13:53:16 +0100

    Update TypesPokemonsDAO

--- src/DAO/TypesPokemonsDAO.php
+++ src/DAO/TypesPokemonsDAO.php
@@ -25,7 +25,33 @@ class TypesPokemonsDAO extends DAO
     public function setTypeDAO(TypesDAO $typeDAO) {
         $this->typeDAO = $typeDAO;
     }
+    
+    /**
+     * Return a list of all pokemons for a selected Type.
+     *
+     * @param string $codeType The code type.
+     * @return array A list of all pokemons corresponding to the code type.
+     */
+    public function findAllByType($codeType) {
 
-    /* TODO : complete the 7th iteration */
-    protected function buildDomainObject($row) { }
+        //TODO method find in Type.DAO
+        $type = $this->typeDAO->find($codeType);
+
+        $sql = "select * from Pokemons p 
+                            JOIN TypesPokemons t ON t.idpkm = p.idpkm 
+                            where t.codeType=? order by p.nom_pkm";
+        $result = $this->getDb()->fetchAll($sql, array($codeType));
+
+        $allPokemons = array();
+        foreach ($result as $row) {
+            $idpkm = $row['idpkm'];
+            $aPokemon = $this->$pokemonDAO->buildDomainObject($row);
+            $allPokemons [$idpkm] = $aPokemon;
+        }
+        return $allPokemons;
+    }
+
+    protected function buildDomainObject($row) { 
+       //TODO
+    }
 }
